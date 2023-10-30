9d83566d4eee75a7b6deae40ce62d2002c747a3f
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-10-30 11:05:14 +0700

    add data table in aproval page

--- resources/views/sistemPenawaran/approval/index.blade.php
+++ resources/views/sistemPenawaran/approval/index.blade.php
@@ -1,4 +1,17 @@
 @section('headerScript')
+<!-- third party css -->
+<link href="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<link
+    href="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<link
+    href="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<link
+    href="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-select-bs5/css//select.bootstrap5.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<!-- third party css end -->
 @endsection
 
 @extends('sistemPenawaran.template.index')
@@ -11,7 +24,57 @@
 
             <div class="row">
                 <div class="col-12">
-                    <h1>Halaman Approval</h1>
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="mt-0 header-title">Default Example</h4>
+                            <p class="text-muted font-14 mb-3">
+                                DataTables has most features enabled by default, so all you need to do to use it with
+                                your own tables is to call the construction function: <code>$().DataTable();</code>.
+                            </p>
+
+                            <table id="datatable" class="table table-bordered dt-responsive table-responsive nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Position</th>
+                                        <th>Office</th>
+                                        <th>Age</th>
+                                        <th>Start date</th>
+                                        <th>Salary</th>
+                                    </tr>
+                                </thead>
+
+
+                                <tbody>
+                                    @for($i=0; $i<=10; $i++) <tr>
+                                        <td>Tiger Nixon</td>
+                                        <td>System Architect</td>
+                                        <td>Edinburgh</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Garrett Winters</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>63</td>
+                                            <td>2011/07/25</td>
+                                            <td>$170,750</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ashton Cox</td>
+                                            <td>Junior Technical Author</td>
+                                            <td>San Francisco</td>
+                                            <td>66</td>
+                                            <td>2009/01/12</td>
+                                            <td>$86,000</td>
+                                        </tr>
+                                        @endfor
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -20,5 +83,48 @@
 </div>
 @endsection
 
+
 @section('pageScript')
+<!-- third party js -->
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}">
+</script>
+<script
+    src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}">
+</script>
+<script
+    src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}">
+</script>
+<script
+    src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons/js/buttons.flash.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/datatables.net-select/js/dataTables.select.min.js') }}">
+</script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+<script src="{{ asset('templateAdmin/Admin/dist/assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+<!-- third party js ends -->
+
+<script>
+    $(document).ready(function () {
+        $("#datatable").DataTable({
+            // responsive: true;
+            autoWidth: false,
+            processing: true,
+            responsive: false,
+
+        });
+    });
+    
+</script>
 @endsection
