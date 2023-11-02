8131ec895aa2edb3972286d1d651768682d6f09f
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-11-02 14:23:30 +0700

    update library dan cdn load createmilestone page

--- resources/views/projects/createMilestone.blade.php
+++ resources/views/projects/createMilestone.blade.php
@@ -1,5 +1,18 @@
 @extends('template.index')
 
+{{-- headescript section --}}
+@section('headerScript')
+    <!-- dropzone file upload css -->
+    <link href="{{ asset('templateAdmin/Admin/dist/assets/libs/dropzone/min/dropzone.min.css') }}" rel="stylesheet"
+    type="text/css" />
+    <link href="{{ asset('templateAdmin/Admin/dist/assets/libs/dropify/css/dropify.min.css') }}" rel="stylesheet"
+    type="text/css" />
+
+    {{-- flatepckr date time css --}}
+    <link href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" rel="stylesheet" type="text/css">
+@endsection
+
+{{-- content section --}}
 @section('content')
     <div class="content-page">
         <div class="content">
@@ -81,6 +94,13 @@
     </div>
 @endsection
 
-{{-- script js halaman create milestone --}}
+{{-- pagescript section --}}
 @section('pageScript')
+    {{-- Plugins js for file upload-dropify dan dropzone --}}
+    <script src="{{ asset('templateAdmin/Admin/dist/assets/libs/dropzone/min/dropzone.min.js') }}"></script>
+    <script src="{{ asset('templateAdmin/Admin/dist/assets/libs/dropify/js/dropify.min.js') }}"></script>
+    <script src="{{ asset('templateAdmin/Admin/dist/assets/js/pages/form-fileuploads.init.js') }}"></script>
+
+    {{-- flatpckr date time js --}}
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>  
 @endsection
