05b952e15583d74a340140f596c777e20ecbd5a3
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-11-03 16:32:12 +0700

    benerin nama perusahaan di footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col text-end">
-                2023 &copy; Created by <a href="https://www.trafoindonesia.com/">PT Trafindo Prima Perkasa</a>
+                2023 &copy; Created by <a href="https://www.trafoindonesia.com/">PT Trafoindo Prima Perkasa</a>
             </div>
         </div>
     </div>
