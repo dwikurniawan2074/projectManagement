a280fc834f4b5464c9d17e1059eb37d2a886ccb8
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-11-14 14:01:47 +0700

    edit migrations table record document

--- database/migrations/2023_11_06_101550_create_record_documents_table.php
+++ database/migrations/2023_11_06_101550_create_record_documents_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('record_documents', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->uuid('project_id');
             $table->date('submitted_date');
             $table->date('due_date');
             $table->text('description');
