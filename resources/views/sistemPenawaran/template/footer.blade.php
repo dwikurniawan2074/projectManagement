0d8b242b737a27c1098484d15be20e10ce98a143
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-11-14 14:52:41 +0700

    rapihin dikit

--- resources/views/sistemPenawaran/template/footer.blade.php
+++ resources/views/sistemPenawaran/template/footer.blade.php
@@ -1,20 +1,9 @@
-<!-- Footer Start -->
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col text-end">
-                2023 &copy; Created by <a href="https://www.trafoindonesia.com/">PT Trafindo Prima Perkasa</a>
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col text-end">
+                    2023 &copy; Created by <a href="https://www.trafoindonesia.com/">PT Trafindo Prima Perkasa</a>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
-<!-- end Footer -->
-
-{{-- </div> --}}
-<!-- ============================================================== -->
-<!-- End Page content -->
-<!-- ============================================================== -->
-
-
-{{-- </div> --}}
-<!-- END wrapper -->
+    </footer>
