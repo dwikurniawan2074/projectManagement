fd126de83896be9cf43282497454921f40f0d745
Muhammad Asyam Dirhamsyah <muhammadasyad173@gmail.com>
2023-11-20 10:04:11 +0700

    ...

--- resources/views/customer/createCustomerContact.blade.php
+++ resources/views/customer/createCustomerContact.blade.php
@@ -9,36 +9,26 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
-                            <div class="card-body">
-                                <form action="{{ route('customerContact.store') }}" method="POST"
-                                    enctype="multipart/form-data" class="parsley-examples" novalidate="">
+                            <div class="card-body" id="customer-contact-form">
+                                <form action="{{ route('customerContact.store') }}" method="POST">
                                     @csrf
-
-                                    <div class="mb-3">
-                                        <label for="companyName" class="form-label">Company<span
-                                                class="text-danger">*</span></label>
-                                        <input type="text" name="companyName" parsley-trigger="change" required
-                                            placeholder="Company Name Generated dari parent id" class="form-control"
-                                            id="companyName" readonly>
-                                    </div>
                                     <div class="mb-3">
                                         <label for="name" class="form-label">Name<span
                                                 class="text-danger">*</span></label>
                                         <input type="text" name="name" parsley-trigger="change" required=""
-                                            placeholder="Enter Name" class="form-control" id="userName">
+                                            placeholder="Enter Name" class="form-control" id="name">
                                     </div>
                                     <div class="mb-3">
-                                        <label for="companyName" class="form-label">Phone<span
+                                        <label for="phone" class="form-label">Phone<span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" name="name" parsley-trigger="change" required=""
-                                            placeholder="Enter Phone Number" class="form-control" id="userName">
+                                        <input type="text" name="phone" parsley-trigger="change" required=""
+                                            placeholder="Enter Phone Number" class="form-control" id="phone">
                                     </div>
                                     <div class="text-end">
-                                        <a href="{{ route('customer.index') }}" class="btn btn-secondary waves-effect">
-                                            <button type="button" class="btn btn-secondary waves-effect">Cancel</button>
-                                        </a>
+                                        <a href="{{ route('customerContact.index') }}"
+                                            class="btn btn-secondary waves-effect">Cancel</a>
                                         <button class="btn btn-save waves-effect waves-light px-4" type="submit"
-                                            onclick="saveConfirmation()">Save</button>
+                                            id="submitButton">Save</button>
                                     </div>
                                 </form>
                             </div>
@@ -54,3 +44,9 @@
 {{-- script halaman create customers contact --}}
 @section('pageScript')
 @endsection
+{{-- <a href="{{ route('customerContact.index') }}"
+                                            class="btn btn-secondary waves-effect">
+                                            <button type="button" class="btn btn-secondary waves-effect">Cancel</button>
+                                        </a>
+                                        <button class="btn btn-save waves-effect waves-light px-4" type="submit"
+                                            onclick="saveConfirmation()">Save</button> --}}
