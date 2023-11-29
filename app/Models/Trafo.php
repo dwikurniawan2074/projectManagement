498bd867bc813fc42aa59c19d78d48092339b7ba
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-11-29 16:26:24 +0700

    tambah model trafo

--- app/Models/Trafo.php
+++ app/Models/Trafo.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class Trafo extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
     protected $table = 'trafo';
     protected $primaryKey = 'id';
     protected $fillable = [
@@ -15,7 +16,7 @@ class Trafo extends Model
         'merk',
         'capacity',
         'no_seri',
-        'year',
+        'tahun',
     ];
 
 
