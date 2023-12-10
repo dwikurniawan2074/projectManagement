95deac985f784b4db6aedfc91cebd39084f0383d
petersonhz25 <mahez.pradana2070@students.unila.ac.id>
2023-12-10 18:51:35 +0700

    #update

--- resources/views/customLogin.blade.php
+++ resources/views/customLogin.blade.php
@@ -2,22 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Login - Project Management</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="Coder-themes" name="author" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description"/>
+    <meta content="Coder-themes" name="author"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('images/logo_trafindo_only.png') }}">
 
     <!-- App css -->
 
     <link href="{{ asset('templateAdmin/Admin/dist/assets/css/app.min.css') }}" rel="stylesheet" type="text/css"
-        id="app-style" />
+          id="app-style"/>
 
     <!-- icons -->
-    <link href="{{ asset('templateAdmin/Admin/dist/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('templateAdmin/Admin/dist/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
 
     <style>
         .logo-trafindo {
@@ -28,8 +28,8 @@
         .background-page {
 
             background-image: linear-gradient(rgba(81, 81, 81, 0.233),
-                    rgba(255, 0, 0, 0.45)),
-                url('{{ asset('images/trafindo-workshop.png') }}');
+            rgba(255, 0, 0, 0.45)),
+            url('{{ asset('images/trafindo-workshop.png') }}');
             background-size: cover;
         }
 
@@ -45,74 +45,84 @@
 
 </head>
 
-<body class="loading background-page" >
+<body class="loading background-page">
 
-    <div class="account-pages my-5">
-        <div class="container">
+<div class="account-pages my-5">
+    <div class="container">
 
-            <div class="row justify-content-center">
-                <div class="col-md-8 col-lg-6 col-xl-4">
-                    <div class="text-center">
-                        <a href="index.html">
-                            {{-- <img src="{{ asset('images/logo_trafindo_full.png') }}" alt="" height="100"
-                                class="mx-auto"> --}}
-                        </a>
-                        <p class="text-muted mt-2 mb-4"></p>
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-4">
+                <div class="text-center">
+                    <a href="index.html">
+                        {{-- <img src="{{ asset('images/logo_trafindo_full.png') }}" alt="" height="100"
+                            class="mx-auto"> --}}
+                    </a>
+                    <p class="text-muted mt-2 mb-4"></p>
 
-                    </div>
-                    <div class="card">
-                        <div class="card-body p-4">
+                </div>
+                <div class="card">
+                    <div class="card-body p-4">
 
-                            <div class="text-center mb-4">
-                                <img src="{{ asset('images/logo_trafindo_full.png') }}" alt="logo_trafindo"
-                                    class="logo-trafindo mx-auto" style="max-height: 60px;" loading="lazy">
+                        <div class="text-center mb-4">
+                            <img src="{{ asset('images/logo_trafindo_full.png') }}" alt="logo_trafindo"
+                                 class="logo-trafindo mx-auto" style="max-height: 60px;" loading="lazy">
+                        </div>
+
+                        <form action="{{ route('login') }}" class="parsley-examples" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="emailaddress" class="form-label">Email address<span
+                                        class="text-danger">*</span></label>
+                                <input class="form-control" type="email" name="email" id="emailaddress"
+                                       required="true" parsley-trigger="change" placeholder="Enter your email">
+                                @error('email')
+                                <div class="text-danger mt-2">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                             </div>
 
-                            <form action="{{ route('login') }}" class="parsley-examples" method="POST">
-                                @csrf
-                                <div class="mb-3">
-                                    <label for="emailaddress" class="form-label">Email address<span
-                                            class="text-danger">*</span></label>
-                                    <input class="form-control" type="email" name="email" id="emailaddress"
-                                        required="" parsley-trigger="change" placeholder="Enter your email">
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password<span
+                                        class="text-danger">*</span></label>
+                                <input class="form-control" type="password" required="" name="password"
+                                       id="password" parsley-trigger="change" placeholder="Enter your password">
+                                @error('password')
+                                <div class="text-danger mt-2">
+                                    {{ $message }}
                                 </div>
+                                @enderror
+                            </div>
 
-                                <div class="mb-3">
-                                    <label for="password" class="form-label">Password<span
-                                            class="text-danger">*</span></label>
-                                    <input class="form-control" type="password" required="" name="password"
-                                        id="password" parsley-trigger="change" placeholder="Enter your password">
-                                </div>
+                            <div class="mb-3 d-grid text-center">
+                                <button class="btn btn-login round-20" type="submit"> Log In</button>
+                            </div>
 
-                                <div class="mb-3 d-grid text-center">
-                                    <button class="btn btn-login round-20" type="submit"> Log In </button>
-                                </div>
+                            <div class="row mt-3">
+                                <div class="col-12 text-center">
+                                    <p><a href="#" class="text-muted ms-1"><i
+                                                class="fa fa-lock me-1"></i>Forgot your password?</a></p>
+                                    <p class="text-muted">Don't have an account? <a
+                                            href="https://wa.me/628111260360" class="text-dark ms-1"
+                                            target="_blank"><b>Contact Admin</b></a></p>
+                                </div> <!-- end col -->
+                            </div>
+                            <!-- end row -->
+                        </form>
 
-                                <div class="row mt-3">
-                                    <div class="col-12 text-center">
-                                        <p> <a href="#" class="text-muted ms-1"><i
-                                                    class="fa fa-lock me-1"></i>Forgot your password?</a></p>
-                                        <p class="text-muted">Don't have an account? <a
-                                                href="https://wa.me/628111260360" class="text-dark ms-1"
-                                                target="_blank"><b>Contact Admin</b></a></p>
-                                    </div> <!-- end col -->
-                                </div>
-                                <!-- end row -->
-                            </form>
+                    </div> <!-- end card-body -->
+                </div>
+                <!-- end card -->
 
-                        </div> <!-- end card-body -->
-                    </div>
-                    <!-- end card -->
-
-                </div> <!-- end col -->
-            </div>
-            <!-- end row -->
+            </div> <!-- end col -->
         </div>
-        <!-- end container -->
+        <!-- end row -->
     </div>
-    <!-- end page -->
+    <!-- end container -->
+</div>
+<!-- end page -->
 
-    @include('template.script')
+@include('template.script')
 
 </body>
 
