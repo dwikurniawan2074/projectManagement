0e00cb881d94c23d9e7804ad69b85b8686d5b1da
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-12-11 14:02:26 +0700

    update fungsi dokumen

--- app/Http/Controllers/SistemPenawaran/ApprovalController.php
+++ app/Http/Controllers/SistemPenawaran/ApprovalController.php
@@ -34,26 +34,28 @@ class ApprovalController extends Controller
         // return $dompdf->stream('print_preview.pdf');
 
         // Create Dompdf instance
-        $dompdf = new Dompdf();
+        // $dompdf = new Dompdf();
 
-        $penawaranPdf = view('sistemPenawaran.approval.pdf')->render();
+        // $penawaranPdf = view('sistemPenawaran.approval.pdf')->render();
 
-        // Load HTML content
-        $dompdf->loadHtml($penawaranPdf);
+        // // Load HTML content
+        // $dompdf->loadHtml($penawaranPdf);
 
-        // Set paper size and orientation (optional)
-        $dompdf->setPaper('A4', 'portrait');
+        // // Set paper size and orientation (optional)
+        // $dompdf->setPaper('A4', 'portrait');
 
-        // Render the HTML as PDF
-        $dompdf->render();
+        // // Render the HTML as PDF
+        // $dompdf->render();
 
-        // Output the generated PDF (temporary file)
-        $pdf = $dompdf->output();
+        // // Output the generated PDF (temporary file)
+        // $pdf = $dompdf->output();
 
-        // Return response with PDF content
-        return response($pdf, 200, [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="generated.pdf"'
-        ]);
+        // // Return response with PDF content
+        // return response($pdf, 200, [
+        //     'Content-Type' => 'application/pdf',
+        //     'Content-Disposition' => 'inline; filename="generated.pdf"'
+        // ]);
+
+        return view('sistemPenawaran.approval.pdf');
     }
 }
