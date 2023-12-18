35c6976ce232229a0c8cc8c97da3d458b66aa5ab
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-12-18 22:47:10 +0700

    count data penawran based on status

--- app/Http/Controllers/SistemPenawaran/DashboardPenawaranController.php
+++ app/Http/Controllers/SistemPenawaran/DashboardPenawaranController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\SistemPenawaran;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Penawaran;
 
 class DashboardPenawaranController extends Controller
 {
     //
     public function index(){
-        return view('sistemPenawaran.dashboardPenawaran');
+        $penawaranTotal = Penawaran::count();
+        $penawaranApproved = Penawaran::where('status', 'approved')->count();
+        $penawaranRejected = Penawaran::where('status', 'rejected')->count();
+        $penawaranWaiting = Penawaran::where('status', 'waiting')->count();
+
+        return view('sistemPenawaran.dashboardPenawaran', compact('penawaranTotal', 'penawaranApproved', 'penawaranRejected', 'penawaranWaiting'));
     }
 }
